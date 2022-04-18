237be2898b98b02d4661c38ecd02f3590b044866
JanZ <janzuiderveld@gmail.com>
2022-04-18 16:19:23 +0200

    ss

--- dream_machine/Python-TCP-Image-Socket/inotify.sh
+++ dream_machine/Python-TCP-Image-Socket/inotify.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 touch Sketch-Simulator/out.log
-MONITORDIR="8081_images0"
+MONITORDIR="Sketch-Simulator/out/to_send"
 inotifywait -m -r -e create --format '%w%f' "${MONITORDIR}" | while read NEWFILE
 do
         echo "${NEWFILE}" >> out.log

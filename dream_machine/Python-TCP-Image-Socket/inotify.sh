142d73cbf9dbca5addedad704ff7fa1ef345b5e5
JanZ <janzuiderveld@gmail.com>
2022-04-18 17:55:39 +0200

    ss

--- dream_machine/Python-TCP-Image-Socket/inotify.sh
+++ dream_machine/Python-TCP-Image-Socket/inotify.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 touch Sketch-Simulator/out.log
-MONITORDIR="Sketch-Simulator/out/to_send"
+MONITORDIR="/workspace/vast_ai/dream_machine/Sketch-Simulator/out/to_send"
 inotifywait -m -r -e create --format '%w%f' "${MONITORDIR}" | while read NEWFILE
 do
-        echo "${NEWFILE}" >> out.log
+        echo "${NEWFILE}" >> Sketch-Simulator/out.log
 done

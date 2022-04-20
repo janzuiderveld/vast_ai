1044ade745762e066ea01c72ad28f86b4a2d81b2
JanZ <janzuiderveld@gmail.com>
2022-04-20 15:05:51 +0200

    ss

--- dream_machine/Python-TCP-Image-Socket/server.py
+++ dream_machine/Python-TCP-Image-Socket/server.py
@@ -67,7 +67,7 @@ class ServerSocket:
                 cv2.imwrite(save_path, decimg)
                 
                 resp_file = utils.wait_new_file(self.output_fp)
-                
+                time.sleep(0.1)
                 if self.esrgan:
                     tmp_folder = "/workspace/vast_ai/dream_machine/temp_upscale"
                     os.makedirs(tmp_folder, exist_ok=True)
@@ -90,7 +90,7 @@ class ServerSocket:
 
                 if self.esrgan:
                     shutil.rmtree(tmp_folder)
-                    
+
                 print('responded image')
 
                 cnt += 1

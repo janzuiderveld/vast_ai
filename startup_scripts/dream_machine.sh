68495bdcd34e468617df984e2fbf7c5b10432f10
JanZ <janzuiderveld@gmail.com>
2022-04-20 13:28:52 +0200

    sssd

--- startup_scripts/dream_machine.sh
+++ startup_scripts/dream_machine.sh
@@ -9,6 +9,8 @@ cd /workspace/vast_ai/dream_machine
 
 python3 -m pip install -r Python-TCP-Image-Socket/requirements.txt
 
+# waits for files send through tcp and saves them in /Users/janzuiderveld/Documents/GitHub/vast_ai/dream_machine/8080_images0
+python3 Python-TCP-Image-Socket/server.py 2>&1 | tee _server_receive.log &
 
 # Real-ESRGAN
 
@@ -19,9 +21,6 @@ python3 -m pip install -r Python-TCP-Image-Socket/requirements.txt
 # python3 setup.py develop
 # wget https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth -P experiments/pretrained_models
 
-
-# waits for files send through tcp and saves them in /Users/janzuiderveld/Documents/GitHub/vast_ai/dream_machine/8080_images0
-python3 Python-TCP-Image-Socket/server.py 2>&1 | tee _server_receive.log &
 
 rm -r /workspace/vast_ai/dream_machine/Sketch-Simulator
 git clone https://github.com/janzuiderveld/Sketch-Simulator.git

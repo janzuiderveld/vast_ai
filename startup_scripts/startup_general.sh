2fb2131093ef84856699d56c1431a4a1405ee6bd
JanZ <janzuiderveld@gmail.com>
2022-04-20 17:26:50 +0200

    ffeqf

--- startup_scripts/startup_general.sh
+++ startup_scripts/startup_general.sh
@@ -4,10 +4,10 @@ git clone https://github.com/janzuiderveld/vast_ai.git
 # bash vast_ai/"$1"/"$1".sh
 # bash vast_ai/"$1"/"$1".sh 2>&1 | test.log
 
-apt-get update
-apt-get install ffmpeg libsm6 libxext6 -y
+# apt-get update
+# apt-get install ffmpeg libsm6 libxext6 -y
 
-cd /workspace/vast_ai/dream_machine
+# cd /workspace/vast_ai/dream_machine
 
-python3 -m pip install -r Python-TCP-Image-Socket/requirements.txt
+# python3 -m pip install -r Python-TCP-Image-Socket/requirements.txt
 

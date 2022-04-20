4ec192a1f3b9d5d46ffe44b639388ecba4d27848
JanZ <janzuiderveld@gmail.com>
2022-04-20 11:55:06 +0200

    s

--- meta_scripts/dream_machine.sh
+++ meta_scripts/dream_machine.sh
@@ -1,7 +1,8 @@
 python3 vast_bid.py auto1 dream_machine
 
-echo "Wating 5 minutes before starting local script"
+# echo "Wating 5 minutes before starting local script"
 
-sleep 300
+# sleep 300
+
 
 bash startup_scripts/dream_machine_local.sh 

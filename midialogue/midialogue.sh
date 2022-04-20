c5bca06c61fb843a0b1cb0bcb555e3af5264364a
JanZ <janzuiderveld@gmail.com>
2022-04-20 16:17:17 +0200

    ffff

--- midialogue/midialogue.sh
+++ midialogue/midialogue.sh
@@ -4,11 +4,11 @@ git clone https://github.com/chrisdonahue/LakhNES
 
 python3 - << EOF
 import os
-os.makedirs("/root/LakhNES/model/pretrained/LakhNES", exist_ok=True)
+os.makedirs("/workspace/vast_ai/midialogue/LakhNES/model/pretrained/LakhNES", exist_ok=True)
 EOF
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1ND27trP3pTAl6eAk5QiYE9JjLOivqGsd' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1ND27trP3pTAl6eAk5QiYE9JjLOivqGsd" -O /root/LakhNES/model/pretrained/model.tar.gz && rm -rf /tmp/cookies.txt
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1WO6guGagqaw22LH32_NEBeavtbaWy_ar' -O /root/LakhNES/data/nesmdb_tx1.tar.gz
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1ND27trP3pTAl6eAk5QiYE9JjLOivqGsd' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1ND27trP3pTAl6eAk5QiYE9JjLOivqGsd" -O /workspace/vast_ai/midialogue/LakhNES/model/pretrained/model.tar.gz && rm -rf /tmp/cookies.txt
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1WO6guGagqaw22LH32_NEBeavtbaWy_ar' -O /workspace/vast_ai/midialogue/LakhNES/data/nesmdb_tx1.tar.gz
 
 cd /workspace/vast_ai/midialogue/LakhNES/model/pretrained
 tar -xf model.tar.gz && rm model.tar.gz

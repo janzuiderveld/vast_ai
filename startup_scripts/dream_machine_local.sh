d1427c028468e0d0c9c19b4bacc7d12579677f9e
JanZ <janzuiderveld@gmail.com>
2022-04-20 10:02:11 +0200

    upppp

--- startup_scripts/dream_machine_local.sh
+++ startup_scripts/dream_machine_local.sh
@@ -14,8 +14,6 @@ cd dream_machine
 # python3 Python-TCP-Image-Socket/client_receive_socket.py 2>&1 | tee _client_receive.log &
 
 
-sleep 180
-
 # until [ -f /tmp/examplefile.txt ]
 # do
 #      sleep 5
@@ -23,16 +21,13 @@ sleep 180
 # echo "SERVER READY"
 
 
-echo 1
-
 # looks for files to be added to /workspace/vast_ai/dream_machine/Sketch-Simulator/out.log 
 # (when inotify script is running and detecting files in Sketch-Simulator/out/to_send) 
 # files are copied to out_imgs.
-python3 Python-TCP-Image-Socket/client_pull.py 2>&1 | tee _client_pull.log &
 
-echo 2
 
 # looks for files to appear in /Users/janzuiderveld/Documents/GitHub/vast_ai/dream_machine/in_imgs, sends them over tcp
-python3 Python-TCP-Image-Socket/client_send_socket.py 2>&1 | tee _client_send.log &
+# python3 Python-TCP-Image-Socket/client.py 2>&1 | tee _client_send.log &
+python3 Python-TCP-Image-Socket/client.py 
 
 

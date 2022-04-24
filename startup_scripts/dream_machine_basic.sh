5d8c385c2f09d7b155c29cde94145ac717666156
JanZ <janzuiderveld@gmail.com>
2022-04-24 17:05:25 +0200

    fvdfb

--- startup_scripts/dream_machine_basic.sh
+++ startup_scripts/dream_machine_basic.sh
@@ -2,5 +2,9 @@ cd /workspace
 apt-get install git wget curl tar -y
 git clone https://github.com/janzuiderveld/vast_ai.git
 
+cd /workspace/vast_ai/dream_machine
+rm -r /workspace/vast_ai/dream_machine/Sketch-Simulator
+git clone https://github.com/janzuiderveld/Sketch-Simulator.git
+
 cd /workspace/vast_ai/dream_machine/Sketch-Simulator 
 bash setup.sh | tee ../_sketch_setup.log

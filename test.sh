9c0ec4125004c5525b85101ea6c194686c9d49a2
JanZ <janzuiderveld@gmail.com>
2022-04-25 11:07:30 +0200

    ffe

--- test.sh
+++ test.sh
@@ -1,11 +1,1 @@
-kill -9 $(lsof -t -i:8080)
-
-cmd=`cat midialogue/ssh_pipe.cmd` 
-$cmd > test_ssh.thrash &
-
-sleep 3
-
-echo $(lsof -i:8080)
-
-cd midialogue
-python3 ./Python-TCP-Image-Socket/client.py --dummy 1 2>&1 | tee _client_send.log &
+cd /Users/janzuiderveld/Documents/GitHub/vast_ai/midialogue/midi-utilities/bin

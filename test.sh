c63dd8087e6be60a066a3bbf9d27df3b3a0acde2
JanZ <janzuiderveld@gmail.com>
2022-04-25 12:10:35 +0200

    upp

--- test.sh
+++ test.sh
@@ -1,1 +1,4 @@
 cd /Users/janzuiderveld/Documents/GitHub/vast_ai/midialogue/midi-utilities/bin
+prefix=$"/Users/janzuiderveld/Documents/GitHub/vast_ai/midialogue/midi_in/"
+# ./brainstorm  --in <port> [ --prefix <filename prefix> ] [ --timeout <seconds> ] [ --confirmation <command line> ]
+./brainstorm  --in 0 --prefix $prefix --timeout 2 --confirmation 'echo "saved a midi"'

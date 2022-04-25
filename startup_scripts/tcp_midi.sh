680ffbb7eeee7a37592056c5664f468347a7b569
JanZ <janzuiderveld@gmail.com>
2022-04-25 09:56:47 +0200

    fff

--- startup_scripts/tcp_midi.sh
+++ startup_scripts/tcp_midi.sh
@@ -2,9 +2,6 @@ cd /workspace
 apt-get install git wget curl tar -y
 git clone https://github.com/janzuiderveld/vast_ai.git
 
-apt-get update
-apt-get install ffmpeg libsm6 libxext6 -y
-
 cd /workspace/vast_ai/midialogue
 
 python3 -m pip install -r Python-TCP-Image-Socket/requirements.txt
@@ -12,5 +9,5 @@ python3 -m pip install -r Python-TCP-Image-Socket/requirements.txt
 echo "READY" > /workspace/vast_ai/midialogue/READY.log
 
 # waits for files send through tcp and saves them in /Users/janzuiderveld/Documents/GitHub/vast_ai/dream_machine/8080_images0
-python3 Python-TCP-Image-Socket/server.py --dummy 1 2>&1 | tee _server_receive.log &
+#python3 Python-TCP-Image-Socket/server.py --dummy 1 2>&1 | tee _server_receive.log &
 

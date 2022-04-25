42ec6b11a6bbec22dd30dcf524336df75232c234
JanZ <janzuiderveld@gmail.com>
2022-04-25 08:58:35 +0200

    vvrb

--- meta_scripts/dream_machine.sh
+++ meta_scripts/dream_machine.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# python3 vast_bid.py auto1 dream_machine
+python3 vast_bid.py auto1 dream_machine
 # python3 vast_bid.py a100 dream_machine
 # python3 vast_bid.py best1 dream_machine
-python3 vast_bid.py cheap dream_machine
+# python3 vast_bid.py cheap dream_machine
 # python3 vast_bid.py cheap4 dream_machine
 
 

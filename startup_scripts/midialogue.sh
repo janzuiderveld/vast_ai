ed97d21a7aa5b7d198385c1ee98b48762ba3f633
JanZ <janzuiderveld@gmail.com>
2022-04-25 18:48:54 +0200

    ddvgg

--- startup_scripts/midialogue.sh
+++ startup_scripts/midialogue.sh
@@ -9,7 +9,7 @@ python3 -m pip install pretty_midi
 
 # waits for files send through tcp and saves them in /Users/janzuiderveld/Documents/GitHub/vast_ai/midialogue/midi_in
 # python3 Python-TCP-Image-Socket/server.py 2>&1 | tee _server_receive.log &
-python3 Python-TCP-Image-Socket/server.py 2>&1 | tee _server_receive.log &
+python3 Python-TCP-Image-Socket/server.py  2>&1 | tee _server_receive.log &
 
 cd /workspace/vast_ai/midialogue
 git clone https://github.com/chrisdonahue/LakhNES
@@ -36,5 +36,5 @@ touch /workspace/vast_ai/midialogue/LakhNES/model/__init__.py
 
 cd /workspace/vast_ai/midialogue/LakhNES
 
-python3 startup.py 2>&1 | tee ../_midialogue_server.log
+# python3 startup.py 2>&1 | tee ../_midialogue_server.log
 

1ed5a229a354c6973a2f2854e3ed4379a2365e7a
JanZ <janzuiderveld@gmail.com>
2022-04-28 22:30:06 +0200

    dd

--- startup_scripts/midialogue.sh
+++ startup_scripts/midialogue.sh
@@ -38,5 +38,5 @@ cd /workspace/vast_ai/midialogue/LakhNES
 
 echo "starting LakhNES server"
 
-python3 startup.py
+python3 startup.py | tee -a "$(tty)"
 # stdbuf -o0 python3 startup.py 2>&1 | tee ../_midialogue_server.log

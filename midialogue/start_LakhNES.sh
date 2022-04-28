283b20b10be08c3c9b483f27d7f932df3d904043
JanZ <janzuiderveld@gmail.com>
2022-04-28 23:14:33 +0200

    dd

--- midialogue/start_LakhNES.sh
+++ midialogue/start_LakhNES.sh
@@ -4,4 +4,4 @@ echo "starting LakhNES server"
 
 python3 ../print.py # This is in a different location, but printing works now...??
 
-python3 startup.py
+python3 startup.py | tee -a /workspace/vast_ai/midialogue/LakhNES/logs/startup.log

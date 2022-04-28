6f9ab8c376f83f454b623c5e6a8fc3d995e66a29
JanZ <janzuiderveld@gmail.com>
2022-04-28 16:16:28 +0200

    f

--- meta_scripts/midialogue.sh
+++ meta_scripts/midialogue.sh
@@ -4,6 +4,11 @@ wget https://raw.githubusercontent.com/vast-ai/vast-python/master/vast.py -O vas
 apt-get -y install python3-pip python3-venv
 # yes | apt-get install python3-pip python3-venv
 
+ROOT_DIR=$PWD
+python3 -m venv $ROOT_DIR/midialogue/midialogue_env
+source $ROOT_DIR/midialogue/midialogue_env/bin/activate
+python3 -m pip install requests
+
 python3 vast_bid.py cheap midialogue
 # python3 vast_bid.py auto1_bid midialogue
 

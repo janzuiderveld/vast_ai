7d62758e495ce5f114e1905222ffc18e7ec10324
JanZ <janzuiderveld@gmail.com>
2022-04-28 14:35:53 +0200

    d

--- meta_scripts/midialogue.sh
+++ meta_scripts/midialogue.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+wget https://raw.githubusercontent.com/vast-ai/vast-python/master/vast.py -O vast; chmod +x vast;
 python3 vast_bid.py cheap midialogue
 # python3 vast_bid.py auto1_bid midialogue
 

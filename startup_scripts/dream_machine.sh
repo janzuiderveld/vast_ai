5a7aa028778c35494a33ee651ee5c4bba6c4f3a1
JanZ <janzuiderveld@gmail.com>
2022-04-29 14:04:41 +0200

    fff

--- startup_scripts/dream_machine.sh
+++ startup_scripts/dream_machine.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+source ~/.bashrc
+
 cd /workspace
 apt-get install git wget curl tar -y
 git clone https://github.com/janzuiderveld/vast_ai.git
@@ -14,6 +17,7 @@ python3 Python-TCP-Image-Socket/server.py 2>&1 | tee _server_receive.log &
 
 # Real-ESRGAN
 
+echo "setting up Real-ESRGAN"
 git clone https://github.com/xinntao/Real-ESRGAN.git
 cd /workspace/vast_ai/dream_machine/Real-ESRGAN
 python3 -m pip install basicsr facexlib gfpgan
@@ -21,17 +25,20 @@ python3 -m pip install -r  requirements.txt
 python3 setup.py develop
 wget https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth -P /workspace/vast_ai/dream_machine/Real-ESRGAN/experiments/pretrained_models
 
-
+echo "setting up LakhNES"
+echo "downloading git"
 cd /workspace/vast_ai/dream_machine
 rm -r /workspace/vast_ai/dream_machine/Sketch-Simulator
 git clone https://github.com/janzuiderveld/Sketch-Simulator.git
 
+echo "downloading pretrained model"
 cd /workspace/vast_ai/dream_machine/Sketch-Simulator 
 bash setup.sh | tee ../_sketch_setup.log
 # python3 -m pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 -f https://download.pytorch.org/whl/torch_stable.html | tee ../_sketch_torch_update.log
 
 cd /workspace/vast_ai/dream_machine
 
+echo "starting LakhNES"
 chmod +rwx start_sketch_sim_server.sh
 bash -i start_sketch_sim_server.sh
 

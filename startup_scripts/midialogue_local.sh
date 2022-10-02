7af375ff787d0beb67ae1ac524f5ff8fbce10f74
JanZ <janzuiderveld@gmail.com>
2022-10-02 15:44:39 +0200

    g

--- startup_scripts/midialogue_local.sh
+++ startup_scripts/midialogue_local.sh
@@ -157,6 +157,7 @@ echo "Server ready"
 python3 midi_autoplay.py --midi_out_port $out_port &
 
 # python3 Python-TCP-Image-Socket/client.py &
+cd $ROOT_DIR
 ID=$(./vast show instances --raw | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['id'])")
 ./vast copy $"$ROOT_DIR/midialogue/midi_in" $"$ID:/workspace/vast_ai/midialogue/midi_in"
 ./vast copy $"$ID:/workspace/vast_ai/midialogue/midi_out $"$ROOT_DIR/midialogue/midi_out" 

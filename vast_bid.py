88005b387709f564424827d9e2989008e1ed2e27
Your Name <you@example.com>
2022-11-30 21:41:23 +0100

    slsl

--- vast_bid.py
+++ vast_bid.py
@@ -20,28 +20,29 @@ else:
 
 # run vast shell command "./vast search offers" to get prices 
 if args.bidding_tactic == 'cheap':
-    vast_cmd = './vast search offers -b --raw -o "dph"'
+    vast_cmd = './vast search offers -b "direct_port_count>1" --raw -o "dph"'
 if args.bidding_tactic == 'cheap4':
-    vast_cmd = './vast search offers -b "direct_port_count>0 num_gpus == 4" --raw -o "dph"' 
+    vast_cmd = './vast search offers -b "direct_port_count>1 num_gpus == 4" --raw -o "dph"' 
 if args.bidding_tactic == 'cheap8':
-    vast_cmd = './vast search offers -b "direct_port_count>0 num_gpus == 8" --raw -o "dph"' 
+    vast_cmd = './vast search offers -b "direct_port_count>1 num_gpus == 8" --raw -o "dph"' 
 elif args.bidding_tactic == 'auto1_bid':
-    vast_cmd = './vast search offers -b "direct_port_count>0 num_gpus == 1 gpu_ram > 20" --raw'   
+    vast_cmd = './vast search offers -b "direct_port_count>1 num_gpus == 1 gpu_ram > 20" --raw'   
 elif args.bidding_tactic == 'auto1':
-    vast_cmd = './vast search offers -d "direct_port_count>0 num_gpus == 1 gpu_ram > 20" --raw'   
+    vast_cmd = './vast search offers -d "direct_port_count>1 num_gpus == 1 gpu_ram > 20" --raw'   
 elif args.bidding_tactic == 'best1':
-    vast_cmd = './vast search offers -d "direct_port_count>0 num_gpus == 1 gpu_ram > 20" --raw -o "dlperf-"'   
+    vast_cmd = './vast search offers -d "direct_port_count>1 num_gpus == 1 gpu_ram > 20" --raw -o "dlperf-"'   
 
 vast_output = subprocess.check_output(vast_cmd, shell=True)
 
 # print(vast_output)
 top_sellers = json.loads(vast_output.decode("utf-8"))
 
-for i in range(10):
+for i in range(1):
     print(top_sellers[i]["dph_base"],
              top_sellers[i]["min_bid"],
               top_sellers[i]["dlperf"],
-               top_sellers[i]["gpu_ram"])
+               top_sellers[i]["gpu_ram"],
+                top_sellers[i]["direct_port_count"])
 
 # top_id = top_sellers[0]["id"]
 top_id = top_sellers[0]["id"]
@@ -50,10 +51,11 @@ top_id = top_sellers[0]["id"]
 if args.bidding_tactic == 'cheap' or args.bidding_tactic == "cheap8" or args.bidding_tactic == "cheap4" or args.bidding_tactic == "auto1_bid": 
     bid = top_sellers[0]["min_bid"]
     # vast_book_cmd = f"./vast create instance {top_id} --price {bid} --image {image} --disk 30 --onstart startup_scripts/{args.project_name}.sh"
-    vast_book_cmd = f"./vast create instance " + str(top_id) + " --price " + str(bid) + f" --image {image} --disk 30 --onstart startup_scripts/" + args.project_name + ".sh"
+    vast_book_cmd = f"./vast create instance " + str(top_id) + " --price " + str(bid) + f" --image {image} --disk 30 --direct --onstart startup_scripts/" + args.project_name + ".sh"
 else:
     # vast_book_cmd = f"./vast create instance {top_id} --image {image} --disk 30 --onstart startup_scripts/{args.project_name}.sh"
-    vast_book_cmd = f"./vast create instance " + str(top_id) + f" --image {image} --disk 30 --onstart startup_scripts/" + args.project_name + ".sh"
+    vast_book_cmd = f"./vast create instance " + str(top_id) + f" --image {image} --disk 30 --direct --onstart startup_scripts/" + args.project_name + ".sh"
 
+#if args.direct: vast_book_cmd += " --direct"
 vast_output = subprocess.check_output(vast_book_cmd, shell=True)
 print(vast_output)

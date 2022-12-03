3e281bbb11124390020781966199aaab2854f52d
username <emailaddress@yahoo.com>
2022-12-03 15:02:24 +0100

    kkk

--- meta_scripts/dream_machine.sh
+++ meta_scripts/dream_machine.sh
@@ -7,11 +7,14 @@ case "$(uname -s)" in
     #  curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
     #  python3 get-pip.py
     # venv?
+    export DEVICE=Mac
+
      ;;
 
    Linux)
      echo 'Linux'
      apt-get -y install python3-pip python3-venv lsof
+    export DEVICE=Linux
      ;;
 esac
 
@@ -19,20 +22,20 @@ ROOT_DIR=$PWD
 python3 -m venv $ROOT_DIR/dream_machine/dream_machine_env
 source $ROOT_DIR/dream_machine/dream_machine_env/bin/activate
 
-python3 -m pip install requests
-
 # wget https://raw.githubusercontent.com/vast-ai/vast-python/master/vast.py -O vast; chmod +x vast;
-python3 vast_bid.py auto1 dream_machine
+# python3 vast_bid.py auto1 dream_machine
 # python3 vast_bid.py a100 dream_machine
 # python3 vast_bid.py best1 dream_machine
-# python3 vast_bid.py cheap dream_machine
 # python3 vast_bid.py cheap4 dream_machine
+# python3 vast_bid.py cheap dream_machine
+python3 vast_bid.py cheap_od dream_machine
 
 
 # echo "Wating 60 sec before starting local script"
 # sleep 60
 
 # sudo chmod a+x startup_scripts/dream_machine_local.sh
+echo "starting local script"
 ./startup_scripts/dream_machine_local.sh 
 
 
